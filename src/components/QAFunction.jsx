237c6c8765c39c7d322702f2e21b61e18ee6199f
akshay-ken <akshaybest3120@gmail.com>
2025-06-03 15:37:46 +0530

    tried to change logic for click

--- src/components/QAFunction.jsx
+++ src/components/QAFunction.jsx
@@ -3,11 +3,19 @@ import minusIcon from "../assets/images/icon-minus.svg";
 import { useState } from "react";
 
 export function QAFunction({ info }) {
-  const [activeIndext, setActiveIndex] = useState(null);
+  //   const [activeIndext, setActiveIndex] = useState(null);
 
-  const toggleAnswer = (index) => {
-    setActiveIndex(activeIndext === index ? null : index);
-  };
+  //   const toggleAnswer = (index) => {
+  //     setActiveIndex(activeIndext === index ? null : index);
+  //   };
+
+  const [activeState, setActiveState] = useState(null);
+
+  function handleClick(index) {
+    setActiveState(() => {
+      return activeState === index ? null : index;
+    });
+  }
 
   return (
     <>
@@ -16,20 +24,25 @@ export function QAFunction({ info }) {
           <div
             key={index}
             className="flex items-center justify-between my-4"
-            onClick={() => toggleAnswer(index)}
+            // onClick={() => toggleAnswer(index)}
+            onClick={() => handleClick(index)}
           >
             <p className="font-worksans text-xl font-semibold text-purple-950 hover:text-purple-500 active:text-purple-500">
               {qa.Q}
             </p>
             <img
-              src={activeIndext === index ? minusIcon : plusIcon}
+              //   src={activeIndext === index ? minusIcon : plusIcon}
+              src={activeState === index ? minusIcon : plusIcon}
               className="self-start"
               alt=""
             />
           </div>
-          {activeIndext === index && (
+          {/* {activeIndext === index && (
             <p className="font-worksans text-purple-400 font-normal">{qa.A}</p>
-          )}
+          )} */}
+          {activeState === index ? (
+            <p className="font-worksans text-purple-400 font-normal">{qa.A}</p>
+          ) : null}
         </div>
       ))}
     </>
